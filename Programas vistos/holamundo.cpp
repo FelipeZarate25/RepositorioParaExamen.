695701813f9ee62f506f48832b906440ece03d41
Felipe Zarate <91904347+FelipeZarate25@users.noreply.github.com>
2021-11-23 16:21:03 -0600

    Ejemplo de como agregar un archivo o documento.
    Ejemplos para la demostración de mi examen y que merezco un 10. :)

---
+++ Programas vistos/holamundo.cpp
@@ -0,0 +1,8 @@
+#include <studio.h>
+
+ int main{
+
+printf("\n Hola Mundo!");
+return 0;
+
+}
---
+++ Programas vistos/saludo.cpp
@@ -0,0 +1,9 @@
+#include <conio.h>
+#include <stdio.h>
+
+int main(){
+
+printf("?¿\n Hola Mundo");
+return 0;
+
+}
